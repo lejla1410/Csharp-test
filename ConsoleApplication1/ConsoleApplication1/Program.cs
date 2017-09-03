0a6a20df760d2aa13806eba19d373e2dfd6c0281
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-09-03 14:34:07 +0200

    losowe

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,18 +8,31 @@ namespace ConsoleApplication1
 {
     public class Losowosc
     {
-        static int Randomowe( int min, int max)
+        static void Main(string[] args)
         {
-        Random rnd1 = new Random();
-        int pierwsza = rnd1.Next(min, max);
-        Console.WriteLine(" Podaj pierwszą liczbę:");
-        pierwsza = Console.ReadLine();
-        Random rnd2 = new Random();
-        int druga = rnd2.Next(min, max);
-        Console.WriteLine(" Podaj drugą liczbę:");
-        druga= Console.ReadLine();
-            return pierwsza + druga;
+            int userInpMin = 0;
+            int userInpMax = 0;
+            Input("Type min number: ", ref userInpMin);
+            Input("Type max number: ", ref userInpMax);
+            AddRandomNumbers(userInpMin, userInpMax);
+
+            Console.ReadLine();
+        }
+
+        static void AddRandomNumbers(int min, int max)
+        {
+            Random random = new Random();
+            int num1 = random.Next(min, max);
+            int num2 = random.Next(min, max);
+            Console.WriteLine($"First random number: {num1}. Second random number: {num2}");
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+
+        static void Input(string description, ref int inputVariable)
+        {
+            Console.Write(description);
+            inputVariable = Convert.ToInt32(Console.ReadLine());
+        }
 
     }
     }
-}
