8a746549fb580877ae933d0732bef5c370173b19
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-09-03 13:21:59 +0200

    Wiek i imie

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Jak masz na imie?");
+            string name;
+            name = Console.ReadLine();
+
+            Console.WriteLine("Ile masz lat?");
+            string wiek;
+            wiek = Console.ReadLine();
+
+            Console.WriteLine("Cześć" + name + "fajnie, że masz" + wiek + "lat");
+            Console.ReadLine();
         }
     }
 }
