198b28f92618063d6afd9077e808324f2ff77bf0
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-09-03 14:40:52 +0200

    czysty master

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,16 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Jak masz na imie?");
-            string name;
-            name = Console.ReadLine();
-
-            Console.WriteLine("Ile masz lat?");
-            string wiek;
-            wiek = Console.ReadLine();
-
-            Console.WriteLine("Cześć " + name + " fajnie, że masz " + wiek + " lat");
-            Console.ReadLine();
+           
         }
     }
 }
