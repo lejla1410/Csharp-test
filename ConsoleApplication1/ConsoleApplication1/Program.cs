d55a2fce2c2c5d7418064b837b3e46067f7b6aae
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-09-03 15:30:57 +0200

    przeciazenie

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,23 +7,11 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1
 {
     class Program
-    {
-        static int RandAdd(int num1, int num2)
-    {
-        Random rnd = new Random();
-        int Number1 = rnd.Next(num1, num2);
-        int Number2 = rnd.Next(num1, num2);
-        Console.WriteLine(Number1 + " " + Number2);
-        return Number1 + Number2;
-    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Wpisz proszę zakres dwóch losowych liczb do zsumowania.");
-        int Num1, Num2;
-        while (!((int.TryParse(Console.ReadLine(), out Num1)) && (int.TryParse(Console.ReadLine(), out Num2))))
-            Console.WriteLine("Spróbuj jeszcze raz. Pamiętaj, aby wprowadzić dwie liczby w osobnych linijkach.");
-        Console.WriteLine(RandAdd(Num1, Num2));
+        RandomNumber.RandAdd();
         Console.ReadKey();
     }
 }
-    }
+    
---
+++ ConsoleApplication1/ConsoleApplication1/RandomoweNumery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    internal class RandomNumber
+    {
+            static int RandAdd(int num1, int num2)
+            {
+                Random rnd = new Random();
+                int Number1 = rnd.Next(num1, num2);
+                int Number2 = rnd.Next(num1, num2);
+                Console.WriteLine(Number1 + " " + Number2);
+                return Number1 + Number2;
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Wpisz proszę zakres dwóch losowych liczb do zsumowania.");
+                int Num1, Num2;
+                while (!((int.TryParse(Console.ReadLine(), out Num1)) && (int.TryParse(Console.ReadLine(), out Num2))))
+                    Console.WriteLine("Spróbuj jeszcze raz. Pamiętaj, aby wprowadzić dwie liczby w osobnych linijkach.");
+                Console.WriteLine(RandAdd(Num1, Num2));
+                Console.ReadKey();
+            }
+        public static float RandAdd (float liczba1, float liczba2)
+        }
+    }
+
